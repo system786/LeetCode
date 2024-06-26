96bbef59ec3be16ab7bdd0db69946d9ff3eef058
Ayush Maurya <114686837+system786@users.noreply.github.com>
2024-06-26 14:25:41 +0530

    Time: 2 ms (96.27%), Space: 45.9 MB (40.33%) - LeetHub

---
+++ 1382-balance-a-binary-search-tree/1382-balance-a-binary-search-tree.java
@@ -0,0 +1,35 @@
+class Solution {
+    public TreeNode balanceBST(TreeNode root) {
+        
+        List<Integer> inorder = new ArrayList<>();
+        inOrderTrav(root, inorder);
+        return createBalanceBST(inorder, 0, inorder.size() - 1);
+    }
+
+    private void inOrderTrav(TreeNode root, List<Integer> inorder) {
+        if(root == null) return;
+        inOrderTrav(root.left, inorder);
+        inorder.add(root.val);
+        inOrderTrav(root.right, inorder);
+    }
+
+    private TreeNode createBalanceBST(
+        List<Integer> inorder,
+        int start,
+        int end
+    ) {
+        if(start > end) return null;
+
+        int mid = start + (end - start) / 2;
+
+        TreeNode leftSubtree = createBalanceBST(inorder, start, mid-1);
+        TreeNode rightSubtree = createBalanceBST(inorder, mid+1, end);
+
+        TreeNode node = new TreeNode(
+            inorder.get(mid),
+            leftSubtree,
+            rightSubtree
+        );
+        return node;
+    }
+}
