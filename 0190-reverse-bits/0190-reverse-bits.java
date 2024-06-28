5a2afedc7ec9e8ba9ae2ed2427849a6aa805cdf9
Ayush Maurya <114686837+system786@users.noreply.github.com>
2024-06-28 14:35:05 +0530

    Time: 0 ms (100.00%), Space: 41 MB (98.69%) - LeetHub

---
+++ 0190-reverse-bits/0190-reverse-bits.java
@@ -0,0 +1,21 @@
+public class Solution {
+    public int reverseBits(int n) {
+        for(int i = 0; i < 16; i++)
+        {
+            n = swapBits(n, i, 32-i-1);
+        }
+        return n;
+    }
+
+    private int swapBits(int n, int i, int j)
+    {
+        int a = (n >> i) & 1;
+        int b = (n >> j) & 1;
+
+        if((a ^ b) != 0)
+        {
+            return n ^= (1 << i) | (1 << j);
+        }
+        return n;
+    }
+}
